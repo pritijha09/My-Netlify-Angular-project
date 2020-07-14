c7f47ad9dfbe4b614a041a9c55209d6743fbce93
priti <priti.kumari@primussoft.com>
2020-07-14 21:11:55 +0530

    handout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { BannerComponent } from './banner/banner.component';
+import { HandoutComponent } from './handout/handout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    BannerComponent
+    BannerComponent,
+    HandoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/handout/handout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-handout',
+  templateUrl: './handout.component.html',
+  styleUrls: ['./handout.component.css']
+})
+export class HandoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
